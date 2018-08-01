c89e8076de4851c872df2294ded4de69c959df02
mrschlei <mrschlei@umich.edu>
2018-08-01 13:20:45 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -23,7 +23,8 @@ c_rehash /etc/ssl/certs >/dev/null
 
 a2enmod ssl
 a2enmod include
-a2ensite default-ssl 
+#uncomment line below later
+#a2ensite default-ssl 
 
 #cd /var/www/html
 #drush @sites cc all --yes
